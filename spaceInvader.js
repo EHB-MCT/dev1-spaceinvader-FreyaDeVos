d3034cdb4d3855e9df89d943654422da9c90857c
Freya De Vos <freya.de.vos@student.ehb.be>
2022-10-24 13:52:18 +0200

    spaceinvader

--- spaceInvader.js
+++ spaceInvader.js
@@ -1,3 +1,34 @@
 "use strict";
 
-alert("Draw your space invader here");
+
+drawspaceInvader();
+
+function drawspaceInvader() {
+
+    let canvas = document.querySelector("canvas");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    let context = canvas.getContext("2d");
+
+    //background
+    context.fillStyle = "beige";
+    context.beginPath();
+    context.rect(50, 50, 350, 350);
+    context.fill();
+
+    //figuuuVoorgrond
+    context.beginPath();
+    context.fillStyle = "orange";
+    context.rect(100, 100, 250, 150);
+    context.rect(150, 250, 150, 100);
+    context.fill();
+
+    //ogen en mond 
+    context.beginPath();
+    context.fillStyle = "beige";
+    context.rect(150, 150, 50, 50);
+    context.rect(250, 150, 50, 50);
+    context.rect(200, 200, 50, 100);
+    context.fill();
+
+}
